9b5f7abe3a9ee1883b2c1d4d8a20463564db6e62
Sven Jacobs <github@svenjacobs.com>
2023-10-26 13:08:54 +0200

    fix: Use pkgx in GHA workflow

--- build.sh
+++ build.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env sh
-
 mkdir build 2>/dev/null
 rm -rf build/*
 cp -r src/* build
-
-COMMAND="html-minifier"
-if command -v pkgx &> /dev/null
-then
-    COMMAND="pkgx npx --yes $COMMAND --"
-fi
-
-$COMMAND \
+pkgx npx --yes html-minifier@4.0.0 -- \
     --collapse-whitespace \
     --remove-comments \
     --minify-css true \
