330da4e1ee3aa9f5e7114163afd0d14d082ec3c5
Sven Jacobs <github@svenjacobs.com>
2024-02-19 13:52:00 +0100

    fix: build script (#7)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 mkdir build 2>/dev/null
 rm -rf build/*
 cp -r src/* build
-pkgx npx --yes html-minifier@4.0.0 -- \
+pkgx npx --yes html-minifier@4.0.0 \
   --remove-comments \
   --minify-css true \
   --minify-js true \
